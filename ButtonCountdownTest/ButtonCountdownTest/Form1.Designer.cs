e0071a74adc8ba31adc10c794349bf6dad8d8b04
Mohammad Rustamovich Edaev <127394372+DNMRE@users.noreply.github.com>
2024-05-07 11:49:21 +0200

    Add files via upload

---
+++ ButtonCountdownTest/ButtonCountdownTest/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ButtonCountdownTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickBtn = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clickBtn
+            // 
+            this.clickBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clickBtn.Location = new System.Drawing.Point(288, 242);
+            this.clickBtn.Name = "clickBtn";
+            this.clickBtn.Size = new System.Drawing.Size(115, 30);
+            this.clickBtn.TabIndex = 0;
+            this.clickBtn.Text = "Click me!";
+            this.clickBtn.UseVisualStyleBackColor = true;
+            this.clickBtn.Click += new System.EventHandler(this.clickBtn_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(431, 251);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(19, 13);
+            this.outputLabel.TabIndex = 1;
+            this.outputLabel.Text = "10";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.clickBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickBtn;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ ButtonCountdownTest/ButtonCountdownTest/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonCountdownTest
+{
+    public partial class Form1 : Form
+    {
+        int TimesClicked = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clickBtn_Click(object sender, EventArgs e)
+        {
+            TimesClicked = TimesClicked - 1;
+            outputLabel.Text = TimesClicked.ToString();
+
+            if (TimesClicked < 0)
+            {
+                Close();
+            }
+        }
+    }
+}
